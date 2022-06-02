85d3d9664011f5126be9251b6e74dab401a6a7b6
Lucero Gómez <lgomez@innmortal.com>
2022-06-02 16:14:56 +0000

    ih-front-structure: creación de la estructura

---
+++ src/app/plot/discardered/discardered.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-discardered',
+  templateUrl: './discardered.component.html',
+  styleUrls: ['./discardered.component.scss']
+})
+export class DiscarderedComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
