7828cbeb33b65351e900b83bc0898fd60679f832
Lucero Gómez <lgomez@innmortal.com>
2022-06-06 20:12:03 +0000

    Feature/ih sidebar

--- src/app/modules/user/home/home.module.ts
+++ src/app/modules/user/home/home.module.ts
@@ -3,17 +3,24 @@ import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home.component';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
-
+import { HomeRoutingModule } from './home-routing.module';
+import { ReviewComponent } from '../review/review.component';
+import { DiscarderedComponent } from '../discardered/discardered.component';
+import { MainComponent } from '../main/main.component';
 
 
 @NgModule({
   declarations: [
     HomeComponent,
     SidebarComponent,
-    NavbarComponent
+    NavbarComponent,
+    ReviewComponent,
+    DiscarderedComponent,
+    MainComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    HomeRoutingModule
   ]
 })
 export class HomeModule { }
