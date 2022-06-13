a13ef751a308d5bc8316535a791fe62aefa14940
lvirrueta <lvirrueta@innmortal.com>
2022-06-12 22:29:52 -0500

    feature/front/ih-bindData: data binded on review

--- client-spectraltools/src/app/modules/user/review/components/info/info.component.ts
+++ client-spectraltools/src/app/modules/user/review/components/info/info.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
+import { IPlaguePlot } from 'src/app/core/models/IPlaguePlot';
+import { PlaguePlotService } from 'src/app/core/services/plaguePlot.service';
 import { CropInfoInterface } from '../../../interfaces/cropinfo.interface';
 
 @Component({
@@ -7,12 +10,18 @@ import { CropInfoInterface } from '../../../interfaces/cropinfo.interface';
   styleUrls: ['./info.component.scss']
 })
 export class InfoComponent {
-
+  public plaguePlot!: Observable<IPlaguePlot>;
   cropInformation: CropInfoInterface = {
     typecrop: 'Maíz',
     phenologicStage: 'Floración'
   };
 
-  constructor() { }
+  constructor(
+    private plaguePlotService: PlaguePlotService
+  ) {}
+
+  ngOnInit(): void {
+    this.plaguePlot = this.plaguePlotService.getPlaguePlot();
+  }
 
 }
